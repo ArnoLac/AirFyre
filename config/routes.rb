b089bce700072f5b82811113eb01d9baeaebd814
Arnaud Lacombe <arnaud.lacombe@whisp.tech>
2019-08-19 17:18:09 +0200

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
-  resources :dj_profiles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  resources :users do
+    resources :dj_profiles
+  end
 end
