76800c0a581669e52e46003b388076636a0d7aa8
Lena Maglo <maglo.lena@gmail.com>
2019-08-19 18:45:46 +0200

    added the show and the index for DJ. Tested locally and it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+DjProfile.create(stage_name:"leni", user_id: 1, genre: "jazz", bio:"lele")
+
+
