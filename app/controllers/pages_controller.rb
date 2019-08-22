2b607db82e95d10de3ed64b0e0f576b564823d77
Eugenie Inbona <e.inbona@gmail.com>
2019-08-22 15:06:00 +0200

    ajout de photos, et début dimplementation du random method dans la landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @dj_profiles = DjProfile.first(21)
+    # if params search, get specific profiles with where genres inlude celui cherche
   end
 end
