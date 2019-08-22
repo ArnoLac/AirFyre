7a8aa45879085a8dddb3e6f29f845a3974202bed
Lena Maglo <maglo.lena@gmail.com>
2019-08-22 16:44:59 +0200

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,16 @@ if (document.querySelector("#user-form")) {
 if (document.querySelector("#picture-form")) {
   toggleForm("#picture-btn", "#picture-form");
 }
+
+const removeForm = (btn_id, form_id) => {
+  const btn = document.querySelector(btn_id);
+  const form = document.querySelector(form_id);
+
+  btn.addEventListener('click', (event) => {
+    form.classList.toggle("d-none");
+  });
+}
+
+if (document.querySelector('#dj-booking-form')) {
+  toggleForm("#remove-btn", "#dj-booking-form");
+}
