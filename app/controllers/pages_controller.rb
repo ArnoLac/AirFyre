d4bd7d87509f8dfb7b20bbfc630c044af7e17c87
Arnaud Lacombe <arnaud.lacombe@whisp.tech>
2019-08-23 15:27:49 +0200

    added genre to landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,8 @@ class PagesController < ApplicationController
 
   def home
     if params[:query].nil? == false
-      params[:query] == "All DJs" ? @dj_profiles = DjProfile.all.sample(21) : @dj_profiles = DjProfile.where("'#{params[:query]}' = ANY (genres)").sample(21)
+      @query = params[:query]
+      @query == "All DJs" ? @dj_profiles = DjProfile.all.sample(21) : @dj_profiles = DjProfile.where("'#{params[:query]}' = ANY (genres)").sample(21)
     else
       @dj_profiles = DjProfile.all.sample(33)
     end
